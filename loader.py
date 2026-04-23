0775841c351d8d8e8646b6270d4fd39decba1b66
ibrohimjon75557-eng <ibrohimjon75557@gmail.com>
2026-04-23 09:34:17 +0300

    Create loader.py

---
+++ loader.py
@@ -0,0 +1,6 @@
+from aiogram import Bot, Dispatcher
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
+from config import BOT_TOKEN
+
+bot = Bot(token=BOT_TOKEN)
+dp = Dispatcher(bot, storage=MemoryStorage())
